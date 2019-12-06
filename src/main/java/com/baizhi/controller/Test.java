0653477abd8a86e3f66a02ec06c4963c5c10682a
huangchen <1538248010@qq.com>
2019-12-06 09:27:22 +0800

    测试提交githup2.0

--- src/main/java/com/baizhi/controller/Test.java
+++ src/main/java/com/baizhi/controller/Test.java
@@ -6,4 +6,7 @@ package com.baizhi.controller;
  * version : 1.0
  */
 public class Test {
+    public static void main(String[] args) {
+        System.out.println("测试githup类");
+    }
 }
---
+++ src/main/java/com/baizhi/controller/Test1.java
@@ -0,0 +1,9 @@
+package com.baizhi.controller;
+
+/**
+ * author : 张京斗
+ * create_date : 2019/12/6 9:26
+ * version : 1.0
+ */
+public class Test1 {
+}
